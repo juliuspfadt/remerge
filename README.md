7386bedae1a0406c59e486bed277ad64d1a32212
Julius Pfadt <julius.pfadt@gmail.com>
2023-08-30 12:43:34 +0200

    add delete button and readme

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # remerge
+
+## Shiny app to merge REM data for JASP
+
+[https://juliuspfadt.shinyapps.io/remerge/](https://juliuspfadt.shinyapps.io/remerge/)
+
+**This app allows users to upload two data sets that can then be merged into a single data set**
+
+### Description
+
+When collecting relational event data, researchers often obtain two data sets: 
+1.  A data set that contains the relational event history and 
+2.  a data set that contains the person attributes, often called covariates
+
+Given the implementation of relational event modeling (REM) techniques in JASP and the fact that 
+JASP cannot handle two data sets at once, this app allows to upload an event data set and a covariates data set
+which can then be merged. The merged data set can be downloaded. 
+
+For now, the allowed file types are `.csv`and `.xlsx`, and the merged data set is downloaded as a `.csv` file, 
+which JASP can handle easily. 
+
+Since the app fills the rows of the covariates data set with `NA`s 
+it may be necessary to add `NA` in the "Missing value list" in JASP under Preferences/Settings > Data.
--- app.R
+++ app.R
@@ -8,15 +8,18 @@ source("mergeDf.R")
 # Define UI ----
 ui <- fluidPage(
   titlePanel("Merge event and covariates data sets for REM in JASP"),
+  uiOutput("link"),
   
   sidebarLayout(
     sidebarPanel(
       h4("Upload the event data"),
       fileInput(inputId = "eventData", buttonLabel = "Upload", label = NULL, multiple = FALSE, accept = c(".xlsx", ".csv"), 
                 placeholder = ".csv, .xlsx"),
+      uiOutput("deleteEvDataButton"),
       h4("Upload the covariate data"),
       fileInput(inputId = "covariateData", buttonLabel = "Upload", label = NULL, multiple = FALSE, accept = c(".xlsx", ".csv"),
                 placeholder = ".csv, .xlsx"),
+      uiOutput("deleteCovDataButton"),
       uiOutput("downloadText"), 
       uiOutput("downloadButton")
       ),
@@ -35,36 +38,17 @@ ui <- fluidPage(
 
 # Define server logic ----
 server <- function(input, output) {
-  
-  output$downloadText <- renderUI({
-    if (is.null(mergedDt())) return()
-    h4("Download the merged data")
+
+  url <- a("Github page", href="https://github.com/juliuspfadt/remerge")
+  output$link <- renderUI({
+    tagList("Help:", url)
   })
-  
-  output$downloadButton <- renderUI({
-    if (is.null(mergedDt())) return()
-    downloadButton("downloadData", "Download")
-  })
+
+  ### event data 
   
   output$title1 <- renderUI({
     if (is.null(input$eventData)) return()
     h4("Event data")
-  })
-  
-  output$title2 <- renderUI({
-    if (is.null(input$covariateData)) return()
-    h4("Covariate data")
-  })
-  
-  output$mergeButton <- renderUI({
-    if (is.null(input$covariateData) || is.null(input$eventData)) return()
-    actionButton(inputId = "mergeDt", label = "Merge")
-    
-  })
-  
-  output$mergedTitle <- renderUI({
-    if (is.null(mergedDt())) return()
-    h4("Merged data")
   })
   
   dataset1 <- reactive({
@@ -78,8 +62,31 @@ server <- function(input, output) {
     return(dt)
   })
   
+  # for storing and deleting the datasets
+  mydf <- reactiveValues()
+  
+  observeEvent(input$eventData, {
+    mydf$data1 <- dataset1()
+  })
+  
   output$eventDataOut <- renderDT(options = list(pageLength = 5, dom = "pt"), {
-    dataset1()
+    mydf$data1
+  })
+  
+  output$deleteEvDataButton <- renderUI({
+    if (is.null(mydf$data1)) return()
+    actionButton("deleteEventData", "Remove")
+  })
+  
+  observeEvent(input$deleteEventData, {
+    mydf$data1 <- NULL
+  })
+  
+  
+  ### covariates data
+  output$title2 <- renderUI({
+    if (is.null(input$covariateData)) return()
+    h4("Covariate data")
   })
   
   dataset2 <- reactive({
@@ -93,22 +100,55 @@ server <- function(input, output) {
     return(dt)
   })
   
-  output$covariateDataOut <- renderDT(options = list(pageLength = 5, dom = "pt"), {
-    dataset2()
+  observeEvent(input$covariateData, {
+    mydf$data2 <- dataset2()
   })
   
-  # output$test <- renderText({
-  #   mode(dataset1())
-  # })
+  output$covariateDataOut <- renderDT(options = list(pageLength = 5, dom = "pt"), {
+    mydf$data2
+  })
+  
+  output$deleteCovDataButton <- renderUI({
+    if (is.null(mydf$data2)) return()
+    actionButton("deleteCovariateData", "Remove")
+  })
+  
+  observeEvent(input$deleteCovariateData, {
+    mydf$data2 <- NULL
+  })
+
+  
+  ### merged data
+  output$mergedTitle <- renderUI({
+    if (is.null(mergedDt())) return()
+    h4("Merged data")
+  })
+  
+  output$mergeButton <- renderUI({
+    if (is.null(input$eventData) || is.null(input$covariateData)) return()
+    actionButton(inputId = "mergeDt", label = "Merge")
+    
+  })
   
   mergedDt <- eventReactive(input$mergeDt, {
-    tmp <- mergeDf(dataset1(), dataset2())
+    tmp <- mergeDf(mydf$data1, mydf$data2)
     req(tmp)
     return(tmp)
   })
   
   output$mergedData <- renderDT(options = list(pageLength = 5, dom = "pt"), {
     mergedDt()
+  })
+  
+  ### download
+  output$downloadText <- renderUI({
+    if (is.null(mergedDt())) return()
+    h4("Download the merged data")
+  })
+  
+  output$downloadButton <- renderUI({
+    if (is.null(mergedDt())) return()
+    downloadButton("downloadData", "Download")
   })
   
   output$downloadData <- downloadHandler(
@@ -120,6 +160,13 @@ server <- function(input, output) {
     }
   )
   
+
+  
+
+  
+  
+
+
   
 
 }
